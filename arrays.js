d64adf27dd070560cba79911f1e350f76e6066e7
blake mcmullin <mcmullin.blake@gmail.com>
2017-09-04 14:18:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,6 +8,6 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.push(element);
+  array.unshift(element);
   return array
 }
