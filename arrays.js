c9dbf7b4258bde8038a2f7ede59cfb08c9e62417
blake mcmullin <mcmullin.blake@gmail.com>
2017-09-04 14:03:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
   var array = [];
-  array.push(elemt);
+  array.push(element);
   return array
 }
 
